cd86631973efaceba8a9c1994d0908483b4cb3ef
MinDz204 <160019862+MinDz204@users.noreply.github.com>
2024-06-25 18:34:38 +0700

    config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 8h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/nguyentien.dino'
+    discordLink: 'https://discord.gg/vY2baURxds'
 }
